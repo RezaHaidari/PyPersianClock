fb9e2f12e65c8144094e352f674a741a532b840e
Reza <shoosh2007@gmail.com>
2016-06-14 11:25:54 -0700

    more cleaner

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ import arabic_reshaper
 import time
 from kivy.app import App 
 from kivy.core.text import LabelBase
-from kivy.core.window import Window 
+from kivy.core.window import Window
 from kivy.utils import get_color_from_hex
 from kivy.clock import Clock
 from kivy.properties import ObjectProperty
@@ -21,14 +21,14 @@ class ClockLayout(BoxLayout):
 class ClockApp(App):
 	sw_started = False
 	sw_seconds = 0
-	
+
 
 	def start_stop(self):
 		self.root.ids.start_stop.text = (self.reshaper(u'شروع')
 			if self.sw_started else self.reshaper(u'ایست'))
 		self.sw_started = not self.sw_started
 
-			
+
 	def reset(self):
 		if self.sw_started:
 			self.root.ids.start_stop.text = self.reshaper(u'شروع')
@@ -36,7 +36,7 @@ class ClockApp(App):
 		self.sw_seconds = 0
 		self.root.ids.stopwatch.text = (
 			'%02d:%02d.[size=40] %02d [/size]' %
-			(00,00, 
+			(00,00,
 			00))
 
 
@@ -47,7 +47,7 @@ class ClockApp(App):
 			minutes, seconds = divmod(self.sw_seconds, 60)
 			self.root.ids.stopwatch.text = (
 			'%02d:%02d.[size=40] %2d [/size]' %
-			(int(minutes),int(seconds), 
+			(int(minutes),int(seconds),
 			int(seconds*100%100)))
 
 
@@ -65,14 +65,6 @@ class ClockApp(App):
 	def reshaper(self, text):
 		reshaped_text = arabic_reshaper.reshape(text)
 		return get_display(reshaped_text)
-	
-
-
-
-
-
-
-
 
 if __name__ == '__main__':
 
